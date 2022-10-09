6dd8a28b1567a6962a46cf6834a6e247a50f6c6c
Manuel Kipfer <manuel.kipfer1@swisscom.com>
2022-10-09 22:42:44 +0200

    Add log Pass missing arg

--- files/srv/backup.js
+++ files/srv/backup.js
@@ -1,11 +1,14 @@
 const fs = require('fs-extra');
 const moment = require('moment');
 
-fs.watch('/astroneer/Astro/Saved/SaveGames', () => {
+fs.ensureDirSync('/astroneer/Astro/Saved/SaveGames');
+
+fs.watch('/astroneer/Astro/Saved/SaveGames', (eventType, filename) => {
+  console.log('BACKUP TRIGGERED', eventType, filename);
   const date = moment().format();
   const folder = `/backup/${date}`;
 
-  fs.ensureDirSync();
+  fs.ensureDirSync(folder);
 
   fs.cpSync('/astroneer/Astro/Saved/SaveGames', folder, { recursive: true });
 });
