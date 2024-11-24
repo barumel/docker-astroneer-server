d49d18e1fb6ee1fb5725823f78fc1f08a006a89e
Manuel Kipfer <manuel.kipfer@bluewin.ch>
2024-11-24 22:01:02 +0100

    Argh...

--- files/srv/src/lib/Config.js
+++ files/srv/src/lib/Config.js
@@ -182,7 +182,9 @@ function AstroServerConfig() {
   return Object.freeze({
     hasEnvVar,
     getEnvVar,
-    getPublicIp,
+    resolveDomainName,
+    resolvePublicIpAddress,
+    getIpAddress,
     update
   });
 }
